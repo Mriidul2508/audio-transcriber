e60e866f0d560c47cc93f4da59b95d19203256cc
Mriidul2508 <113883038+Mriidul2508@users.noreply.github.com>
2026-04-24 18:11:11 +0530

    Update LogoutButton.tsx

--- src/app/dashboard/LogoutButton.tsx
+++ src/app/dashboard/LogoutButton.tsx
@@ -1,18 +1,17 @@
 "use client";
-import { authClient } from "../../lib/auth-client";
+
+import { authClient } from "@/lib/auth-client";
 import { useRouter } from "next/navigation";
 
 export default function LogoutButton() {
   const router = useRouter();
-  
+
   return (
-    <button 
+    <button
       onClick={async () => {
         await authClient.signOut();
         router.push("/login");
-        router.refresh();
       }}
-      className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition"
     >
       Logout
     </button>
