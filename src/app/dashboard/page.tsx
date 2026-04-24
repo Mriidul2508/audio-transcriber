9cb6eeffe6521c4d51a7e694adfd0245339eae1f
Mriidul2508 <113883038+Mriidul2508@users.noreply.github.com>
2026-04-24 17:02:55 +0530

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import { auth } from "@/lib/auth";
+import { auth } from "../../lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import UploadForm from "./UploadForm";
